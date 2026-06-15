13ea5c14365652955ab043e4075b943c4bca1908
Gergely Nagy <gna@r-us.hu>
2026-06-15 04:29:09 +0200

    new implementation for crx v3 files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# crx2zip
-Convert .CRX files to .ZIP to allow for unpacking
+# crx2zip - Convert .CRX files to .ZIP to allow for unpacking
+
+## new implementation for crx v3 files
+
+This is a simple one shot bash script that uses: od and dd to create a .ZIP archive from a .CRX file
+
+### Technical background
+
+The script reads the first 4 bytes, to ensure it is a CRX file, and reads the crx version from the 5th byte, to ensure its version 3 crx file
+For version 3 crx, the header length is stored on the 9-12 bytes of the file. The 4 bytes in swapped order, stripped leading zero bytes, you get the length as 2 byte hex number.
+Adding the format-header length (of 12) to the header length converted to decimal, produces the offset-bytes for **dd**
+Finally it is stripping the offset bytes from the CRX using **dd**, and produces a valid ZIP format file.
+
+### History
+
+- original script by Rowan H - 8/20/2015 ( https://scramble45.github.io/ )
+- Gergely Nagy - 6/15/2026 ( https://github.com/gnanet/crx2zip )
--- crx2zip.sh
+++ crx2zip.sh
@@ -1,45 +1,91 @@
 #!/bin/bash
-# Convert Chromium .CRX file to .ZIP, v1.1
-# Rowan H - 8/20/2015
+#
+# Convert Chromium .CRX file to .ZIP, v1.2
+# new implementation for crx v3 files
+#
+# Gergely Nagy - 6/15/2026 ( https://github.com/gnanet/crx2zip )
+# original script by Rowan H - 8/20/2015 ( https://scramble45.github.io/ )
 
-# This is a simple one shot bash script that uses: hexdump, sed and dd to
-# create a .ZIP archive from a .CRX file
-# my implementation is far from perfect and its possible if PK is
-# referenced sooner in the header the file header than its suppose to be
-# the creation using *dd* may fail.
+# This is a simple one shot bash script that uses: od and dd to
+# create a .ZIP archive from a version 3 .CRX file
+#
+# The new implementation reads the crx version on the 5th byte,
+# and is prepared to handle version 3 file
+# for version 3 crx, the header length is stored on the 9-12 bytes
+# in swapped order, stripped leading zero bytes, you get the length
+# after converting to decimal.
+# Adding the format header length (of 12) to the header length
+# produces the offset bytes for *dd*
 
 
 function checkDeps(){
-echo "Checking for required programs for script to work..."
-type hexdump sed  >/dev/null || echo "Your missing a dependency, make sure you have *sed* and *hexdump* installed."
+    echo "Checking for required programs for script to work..."
+    type od dd  >/dev/null || echo "Your missing a dependency, make sure you have *od* and *dd* installed."
+}
+
+function showUsage(){
+    echo "Usage: $(basename $0) [-h|--help|help|crx-file]"
+    echo "    Convert Chromium .CRX file to .ZIP"
+    echo "    Executing without any argument causes the script to ask for the crx-file name"
+    echo "    Arguments:"
+    echo "       [crx-file] - the CRX filename you want to convert to ZIP"
+    echo "       [-h/--help/help] - shows this help message"
+    exit
 }
 
 checkDeps
 
-
 clear
 
 RED='\033[0;31m'
+RST='\033[0m'
 echo
-printf "${RED}CRX 2 ZIP\n"
+printf "${RED}CRX 2 ZIP${RST}\n"
 echo
-echo
-echo
-read -p "Enter the .CRX file path then press (Enter): " -i "$PWD/" -e crxPath
-echo
+if [[ -f "$1" ]] && [[ "x$(echo "$1" | grep -oE "\.crx$")" != "x" ]]; then
+    crxPath="$1"
+elif [[ "x$(echo "$1" | grep -oE "help|\-h|\-\-help")" != "x" ]]; then
+    showUsage
+else
+    echo
+    echo
+    echo
+    read -p "Enter the .CRX file path then press (Enter): " -i "$PWD/" -e crxPath
+    echo
+    if [[ ! -f "${crxPath}" ]]; then
+        printf "${RED}Failed to open ${crxPath}${RST}\n"
+        exit 1
+    fi
+fi
 
-getPK=$(hexdump -C $crxPath | grep 'PK' | sed -e 's/^\(.\{8\}\).*/\1/' | sed -n '1p')
-echo "PK-Address found at: $getPK"
+isCrx=$(od -A n --endian=little -N 4 -c ${crxPath} | tr -d " ")
+if [[ "x${isCrx}" != "xCr24" ]]; then
+    printf "${RED}No valid CRX format: ${crxPath}${RST}\n"
+    exit 1
+fi
+
+crxVer=$(od -A n --endian=little -j 4 -N 1 -s ${crxPath} | tr -d " ")
+
+if [ ${crxVer} -eq 3 ]; then
+    headerSize=$(od -A n --endian=little -j 8 -N 2 -s ${crxPath} | tr -d " ")
+    getPK=$(( ${headerSize} + 12 ))
+else
+    echo "Current implementation only support version 3 crx, but not version ${crxVer}"
+    exit
+fi
+
+echo "PK Offset found: $getPK"
 
 # Passes address to 'dd' to handle the rest
 
 zipContainerName=$(echo ${crxPath::-4})
 
-echo "Exporting our patched ZIP file to '.' "
+echo "Exporting our patched ZIP file to '${zipContainerName}.zip'"
 
-dd if=$crxPath of=$zipContainerName.zip bs=1 skip=$getPK
+dd if=$crxPath of=$zipContainerName.zip bs=1 iflag=skip_bytes skip=$getPK
 echo
+echo "You can now decompress your ZIP archive:"
 echo
-echo "You can now decompress your ZIP archive..."
+echo "${zipContainerName}.zip"
 
 exit
